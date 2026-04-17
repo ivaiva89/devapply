7ee77db56db7c7c1ad9adfad3a64417f11bf5af6
Iva Kobalava <ivakobalava@gmail.com>
2026-04-18 01:57:04 +0400

    feat: add Chip, DesignCard, DesignButton, DesignInput atoms with barrel export

---
+++ components/design/index.ts
@@ -0,0 +1,5 @@
+export { Chip } from "./chip";
+export type { ChipTone } from "./chip";
+export { DesignCard } from "./card";
+export { DesignButton } from "./button";
+export { DesignInput } from "./input";
